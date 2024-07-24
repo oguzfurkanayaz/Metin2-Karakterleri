b9682b0b9184d395e10c539ab409eef92738fe87
oguzfurkanayaz <oguzfurkanayaz@gmail.com>
2024-07-24 09:56:22 +0300

    Initial Commit

---
+++ SheetView/SheetViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SheetViewApp.swift
+//  SheetView
+//
+//  Created by Ayaz on 24.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct SheetViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
